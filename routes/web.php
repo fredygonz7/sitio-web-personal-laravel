358eb0a09371afd54a026f361624032d5db01acf
FredyGonzalez <fredgonz7@gmail.com>
2020-08-02 12:49:56 -0500

    simplificando rutas con route resource

--- routes/web.php
+++ routes/web.php
@@ -21,20 +21,28 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
 
-// muestra la lista de proyectos
-Route::get('/portfolio', 'ProjectController@index')->name('projects.index');
-// muestra el formulario para crear proyecto
-Route::get('/portfolio/create', 'ProjectController@create')->name('projects.create');
-// muestra el formulario para editar proyecto
-Route::get('/portfolio/{project}/edit', 'ProjectController@edit')->name('projects.edit');
-// envia los datos del formulario para crear proyecto
-Route::post('/portfolio', 'ProjectController@store')->name('projects.store');
-// muestra el formulario para actualizar proyecto
-Route::patch('/portfolio/{project}', 'ProjectController@update')->name('projects.update');
-// muestra el formulario para actualizar proyecto
-Route::delete('/portfolio/{project}', 'ProjectController@destroy')->name('projects.destroy');
-// muesta un proyecto
-Route::get('/portfolio/{project}', 'ProjectController@show')->name('projects.show');
+// // muestra la lista de proyectos
+// Route::get('/portfolio', 'ProjectController@index')->name('projects.index');
+// // muestra el formulario para crear proyecto
+// Route::get('/portfolio/create', 'ProjectController@create')->name('projects.create');
+// // muestra el formulario para editar proyecto
+// Route::get('/portfolio/{project}/edit', 'ProjectController@edit')->name('projects.edit');
+// // envia los datos del formulario para crear proyecto
+// Route::post('/portfolio', 'ProjectController@store')->name('projects.store');
+// // muestra el formulario para actualizar proyecto
+// Route::patch('/portfolio/{project}', 'ProjectController@update')->name('projects.update');
+// // muestra el formulario para actualizar proyecto
+// Route::delete('/portfolio/{project}', 'ProjectController@destroy')->name('projects.destroy');
+// // muesta un proyecto
+// Route::get('/portfolio/{project}', 'ProjectController@show')->name('projects.show');
+
+// utilizando route resource para simplificar las rutas
+Route::resource('projects', 'ProjectController');
+
+// cambiando los link como  estaban antes "portfolio"
+// Route::resource('portfolio', 'ProjectController')
+//     ->parameters(['portfolio' => 'project'])
+//     ->names('projects');
 
 Route::view('/contact', 'contact')->name('contact');
 Route::post('/contact', 'ContactController@store')->name('contact.store');
